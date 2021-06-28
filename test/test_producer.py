5626326c45d1215c6e7cf0fa36569ba8acd073fc
Gus Garcia <pythongus@gmail.com>
2021-06-28 12:24:11 +1000

    Added unit test for producer:

--- test/test_producer.py
+++ test/test_producer.py
@@ -1,4 +1,27 @@
 """
 Unit tests for the producer
 """
-def test_
+import os
+from unittest.mock import MagicMock, patch
+from pyprod.producer import get_data
+
+
+def test_get_data():
+    with patch('pyprod.producer.DATA_FILE', new='./test/data.csv') as mock_data_file:
+        callback = MagicMock()
+        get_data(callback)
+        callback.assert_called_once()
+
+
+def test_get_data_no_data_file():
+    with patch('pyprod.producer.DATA_FILE', new=None) as mock_data_file, \
+            patch('pyprod.producer.exit', callable=raiseError) as sys_exit:
+        try:
+            get_data(None)
+            assert False, 'Exception not raised'
+        except:
+            assert True
+
+
+def raiseError():
+    raise Exception('Exception raised')
